ec89e4448a9db272f112a4bf46f126d6e646bf79
dsmdongly <gdh0608@naver.com>
2018-04-13 11:24:31 +0900

    fixed conflict from main

--- main.py
+++ main.py
@@ -2,33 +2,29 @@ from caspi import cu, gs25, seven_eleven
 from pprint import pprint
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    pprint(seven_eleven.get_stores())
-=======
-    # print('\n - CU PB products - \n')
-    # pprint(cu.get_pb_products())
-    #
-    # print('\n - CU plus event products - \n')
-    # pprint(cu.get_plus_event_products())
+    print('\n - CU PB products - \n')
+    pprint(cu.get_pb_products())
 
-    # print('\n - GS25 youus fresh foods - \n')
-    # pprint(gs25.get_youus_products(gs25.YOUUS_FRESH_FOOD))
-    #
-    # print('\n - GS25 youus diffrent services - \n')
-    # pprint(gs25.get_youus_products(gs25.YOOUS_DIFFRENT_SERVICE))
+    print('\n - CU plus event products - \n')
+    pprint(cu.get_plus_event_products())
 
-    # print('\n - GS25 1+1 products - \n')
-    # pprint(gs25.get_event_products(gs25.ONE_PLUS_ONE))
-    #
-    # print('\n - GS25 2+1 products - \n')
-    # pprint(gs25.get_event_products(gs25.TWO_PLUS_ONE))
+    print('\n - GS25 youus fresh foods - \n')
+    pprint(gs25.get_youus_products(gs25.YOUUS_FRESH_FOODS))
 
-    # print('\n - 7 Eleven 7 selects - \n')
-    # pprint(seven_eleven.get_pb_products())
+    print('\n - GS25 youus diffrent services - \n')
+    pprint(gs25.get_youus_products(gs25.YOOUS_DIFFRENT_SERVICES))
+
+    print('\n - GS25 1+1 products - \n')
+    pprint(gs25.get_event_products(gs25.ONE_PLUS_ONE))
+
+    print('\n - GS25 2+1 products - \n')
+    pprint(gs25.get_event_products(gs25.TWO_PLUS_ONE))
+
+    print('\n - 7 Eleven 7 selects - \n')
+    pprint(seven_eleven.get_pb_products())
 
     print('\n - 7 Eleven 1 + 1 products - \n')
     pprint(seven_eleven.get_event_products(seven_eleven.ONE_PLUS_ONE))
 
     print('\n - 7 Eleven 2 + 1 products - \n')
     pprint(seven_eleven.get_event_products(seven_eleven.TWO_PLUS_ONE))
->>>>>>> d67290bb7b63fbfbde793ed554f948c0d6ab3a61
